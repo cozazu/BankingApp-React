8000ee731c9f1919c8a6ccd4381d80640f523a5d
cozazu <aranzazuarcila@gmail.com>
2024-03-07 23:50:16 -0500

    hw React Intro & Estilos

--- front/vite-project/src/components/NavBar/NavBar.jsx
+++ front/vite-project/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,18 @@
 const NavBar = () => {
     return (
         <div className="{styles.navbarContainer}">
+            {/* <div>
+                Logo
+            </div> */}
             <div>
                 <span>HOME</span>
                 <span>MIS TURNOS</span>
                 <span>ABOUT</span>
                 <span>CONTACTO</span>
             </div>
+            {/* <div>
+                Logo
+            </div> */}
         </div>
     );
 };
